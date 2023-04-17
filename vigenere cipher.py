b18ee1557df3378d535881d01c068d8445073e88
chrismdating1206@gmail.com <christian.dating12>
2023-04-17 20:55:10 +0800

    pseudocode

--- vigenere cipher.py
+++ vigenere cipher.py
@@ -5,3 +5,14 @@ print("\033[90m=" * 80)
 import pyfiglet
 greet = "GOOD DAY!"
 print("\033[92m" + pyfiglet.figlet_format(greet, font = "bubble"))
+
+#Pseudocode
+#Define Variables
+#Conversion
+#User's inputted message
+#User's inputted key
+#MESSAGE
+#KEY
+#ADD
+#MOD
+#CIPHERTEX
