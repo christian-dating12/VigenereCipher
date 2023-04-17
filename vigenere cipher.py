2e95e77188fae885b94b1280cb6b45be06ee4a01
chrismdating1206@gmail.com <christian.dating12>
2023-04-17 20:34:38 +0800

    intro

---
+++ vigenere cipher.py
@@ -0,0 +1,7 @@
+input("\n\033[94mPress Any Key to Start...")
+print("\033[90m=" * 80)
+
+# Intro
+import pyfiglet
+greet = "GOOD DAY!"
+print("\033[92m" + pyfiglet.figlet_format(greet, font = "bubble"))
